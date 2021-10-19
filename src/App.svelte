24279bdff28a080b28b003e0295e332286cc5b24
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-19 23:26:48 +0900

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import SvelteMarkdownBefore from 'svelte-markdown-before';
+	import SvelteMarkdownAfter from 'svelte-markdown-after';
+	export let source = '# test\n\n## test\n';
+</script>
+
+<div>
+<textarea rows="10" bind:value={source} />
+</div>
+
+before:
+<div>
+<SvelteMarkdownBefore {source} />
+</div>
+
+after:
+<div>
+<SvelteMarkdownAfter {source} />
+</div>
